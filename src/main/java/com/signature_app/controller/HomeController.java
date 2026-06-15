6fc83357c09c3364c85a653a7bd09ebdb8547da4
HP <satyajeetpatra43@gmail.com>
2026-06-15 20:58:52 +0530

    Day 1 - Spring Boot setup and PostgreSQL connection

---
+++ src/main/java/com/signature_app/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.signature_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Document Signature App Backend Running!";
+    }
+}
